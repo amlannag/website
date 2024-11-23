64850f9dbf1dd05ad6f157fa9c958fd78aeea0fb
amlannag <amlannag12@gmail.com>
2024-11-23 18:39:57 +1000

    Created Front card

--- app/page.tsx
+++ app/page.tsx
@@ -4,64 +4,182 @@ import { BackgroundBeams } from "@/components/ui/background-beams";
 import Image from 'next/image';
 import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
 import { Timeline } from "@/components/ui/timeline";
+import { PinContainer } from "@/components/ui/3d-pin";
 
 
 const data = [
   {
-    title: "2024",
+    title: "Current",
     content: (
       <div>
-        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          Built and launched Aceternity UI and Aceternity UI Pro from scratch
+        <h3 className="text-lg md:text-2xl mb-10 text-white dark:text-white max-w-4xl font-bold">
+          Work and Project
+        </h3>
+        <div className="grid grid-cols-2 justify-start">
+          <PinContainer
+            title="Blunge.ai"
+            href="https://www.blunge.ai/"
+            
+          >
+            <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[22rem] ">
+              <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                Machine Learning Engineer 
+              </h3>
+              <div className="text-base !m-0 !p-0 font-normal">
+                <span className="text-slate-500 ">
+                Working as a Machine Learning Engineer and researcher at Blunge, where I am developing image-to-video generation capabilities for their fine-tuned Stable Diffusion models.
+                </span>
+              </div>
+              <div className=" w-full rounded-lg mt-4 ">
+                <Image
+                  src="/guidence 9  steps 50.gif"
+                  alt="startup template"
+                  width={400}
+                  height={400}
+                  className="rounded-lg  h-16 md:h-32 lg:h-40 w-full "
+                />
+              </div>
+            </div>
+          </PinContainer>
+          <PinContainer
+            title="Blank Robotics"
+            href="https://x.com/blankrobotics"
+            
+          >
+            <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[22rem] ">
+              <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+                Bipedal Wheeled Robot
+              </h3>
+              <div className="text-base !m-0 !p-0 font-normal">
+                <span className="text-slate-500 ">
+                Currently developing a bipedal wheeled robot. The robot includes a LiDAR detector, an arm module, and a custom reinforcement learning model. Follow our journey!
+                </span>
+              </div>
+              <div className=" w-full rounded-lg mt-4 ">
+                <Image
+                  src="/robot.png"
+                  alt="startup template"
+                  width={600}
+                  height={400}
+                  className="rounded-lg  h-16 md:h-32 lg:h-40 w-full "
+                />
+              </div>
+            </div>
+          </PinContainer>
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "2024 Aug",
+    content: (
+      <div>
+        <h3 className="text-lg md:text-2xl mb-4 text-white dark:text-white max-w-4xl font-bold">
+          Fortus - Software Engineer
+        </h3>
+        <p className="text-neutral-500 dark:text-neutral-200 text-xs md:text-xl font-normal mb-8">
+        Participated in the Lab Accelerator as a software engineer for Fortus, where I contributed to building the frontend and backend of their gym app.
         </p>
       </div>
     ),
   },
   {
-    title: "Early 2023",
+    title: "2024 June",
     content: (
       <div>
-        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          I usually run out of copy, but when I see content this big, I try to
-          integrate lorem ipsum.
-        </p>
-        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          Lorem ipsum is for people who are too lazy to write copy. But we are
-          not. Here are some more example of beautiful designs I built.
+        <h3 className="text-lg md:text-2xl mb-4 text-white dark:text-white max-w-4xl font-bold">
+          UQ Winter Research
+        </h3>
+        <p className="text-neutral-500 dark:text-neutral-200 text-xs md:text-xl font-normal mb-8">
+          Participated in the UQ Winter Research Program under Dr. Miao Xu. Fine-tuned YOLOv8 for object detection and added additional blocks for object tracking. The goal of this project was to quantify the game state for League of Legends and develop a reinforcement learning model to play the game.
         </p>
       </div>
     ),
   },
   {
-    title: "Early 2023",
+    title: " 2024 May",
     content: (
       <div>
-        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          I usually run out of copy, but when I see content this big, I try to
-          integrate lorem ipsum.
-        </p>
-        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          Lorem ipsum is for people who are too lazy to write copy. But we are
-          not. Here are some more example of beautiful designs I built.
+        <h3 className="text-lg md:text-2xl mb-4 text-white dark:text-white max-w-4xl font-bold">
+          UQ Validate Accelerator 
+        </h3>
+        <p className="text-neutral-500 dark:text-neutral-200 text-xs md:text-xl font-normal mb-8">
+        Was one of 10 winners who received funding in UQ’s startup program. I pitched and prototyped a SaaS job interview tool that integrated ChatGPT APIs and image detection models.
         </p>
       </div>
     ),
   },
   {
-    title: "Early 2023",
+    title: "2024 Apr",
     content: (
       <div>
-        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          I usually run out of copy, but when I see content this big, I try to
-          integrate lorem ipsum.
+        <h3 className="text-lg md:text-2xl mb-4 text-white dark:text-white max-w-4xl font-bold">
+          Placed Top 1% in IMC Prosperity Globally
+        </h3>
+        <p className="text-neutral-500 dark:text-neutral-200 text-xs md:text-xl font-normal mb-8">
+          My team and I placed in the top 1% out of 13,000 students globally in the IMC Annual Trading Challenge. In this challenge, we analyzed market data from a wide range of agents and products to curate profitable trading strategies.
         </p>
-        <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
-          Lorem ipsum is for people who are too lazy to write copy. But we are
-          not. Here are some more example of beautiful designs I built.
+      </div>
+    ),
+  },
+  {
+    title: "2023 Dec",
+    content: (
+      <div>
+        <h3 className="text-lg md:text-2xl mb-4 text-white dark:text-white max-w-4xl font-bold">
+          Exit MyIBTutor
+        </h3>
+        <p className="text-neutral-500 dark:text-neutral-200 text-xs md:text-xl font-normal mb-8">
+        Decided to exit MyIBTutor to direct more attention toward technology and machine learning. Over the year, my co-founder and I grew the team to over 30 tutors and taught more than 300 concurrent students. We also partnered with several schools across Queensland to deliver workshops on pedagogy and time management.
         </p>
+      </div>
+    ),
+  },
+  {
+    title: "2023 Sept",
+    content: (
+      <div>
+        <h3 className="text-lg md:text-2xl mb-4 text-white dark:text-white max-w-4xl font-bold">
+          Lead IJM Project as Team Leader
+        </h3>
+        <p className="text-neutral-500 dark:text-neutral-200 text-xs md:text-xl font-normal mb-8">
+        Led a team of 10 consultants to deliver workflow and process mapping solutions for the International Justice Mission. Implemented software solutions to assist with process mapping.
+
+        </p>
+      </div>
+    ),
+  },
+  {
+    title: "2023 Jan",
+    content: (
+      <div>
+        <h3 className="text-lg md:text-2xl mb-4 text-white dark:text-white max-w-4xl font-bold">
+          Co-founded MyIBTutor 
+        </h3>
+        <p className="text-neutral-500 dark:text-neutral-200 text-xs md:text-xl font-normal mb-8">
+        Co-founded MyIBTutor, a tutoring agency specializing in services for International Baccalaureate (IB) students. We delivered lectures at QASMT and grew the team into a small group of dedicated tutors.
+        </p>
+        <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="/46070_MyIBTutor_logo_BJ-01.png"
+              alt="startup template"
+              width={400}
+              height={400}
+              className="rounded-lg object-contain h-16 md:h-32 lg:h-40 w-full "
+            />
+            <Image
+              src="/first lecture 1.png"
+              alt="startup template"
+              width={400}
+              height={400}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
+            />
+            
+          </div>
       </div>
     ),
   }
+  
 ];
 
 
@@ -110,7 +228,7 @@ export default function Home() {
       <div className="max-w-4xl mx-auto p-4 mt-16 flex flex-col z-10 relative">
         <div className="max-w-4xl flex items-start relative">
           <Image
-            src="/image.jpg"
+            src="/pfp.jpg"
             alt="Amlan Nag"
             width={350}
             height={350}
