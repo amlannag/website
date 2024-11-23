87acedb43e327fb13924243ade6725b7fced5f8e
amlannag <amlannag12@gmail.com>
2024-11-24 07:43:36 +1000

    fixing issues

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Image from 'next/image';
 import { AnimatedTooltip } from "@/components/ui/animated-tooltip";
 import { Timeline } from "@/components/ui/timeline";
 import { PinContainer } from "@/components/ui/3d-pin";
-import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
 import { Button } from "@/components/ui/moving-border";
 
 const scrollToContact = () => {
@@ -14,29 +13,6 @@ const scrollToContact = () => {
     contactSection.scrollIntoView({ behavior: "smooth" });
   }
 };
-
-const AceternityLogo = () => {
-  return (
-    <svg
-      width="66"
-      height="65"
-      viewBox="0 0 66 65"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className="h-3 w-3 text-white white:text-white"
-    >
-      <path
-        d="M8 8.05571C8 8.05571 54.9009 18.1782 57.8687 30.062C60.8365 41.9458 9.05432 57.4696 9.05432 57.4696"
-        stroke="currentColor"
-        strokeWidth="15"
-        strokeMiterlimit="3.86874"
-        strokeLinecap="round"
-      />
-    </svg>
-  );
-};
-
-
 
 
 const data = [
